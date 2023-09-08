a325090af34bbdef18bca10d7921373746bcf668
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2023-09-08 18:37:11 +0200

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import Hero from "../Component/Hero";
-import Banner from "../Component/Banner";
-import {Link} from "react-router-dom";
-import TopMeals from "../Component/TopMeals"
-import MealList from "../Component/MealList"
-import RecipeList from "../Component/RecipeList"
-import RecipeDetails from "../Component/RecipeDetails"
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-
+import React from 'react';
+import Hero from '../Component/Hero';
+import Banner from '../Component/Banner';
+import { Link } from 'react-router-dom';
+import TopMeals from '../Component/TopMeals';
+import MealList from '../Component/MealList';
+import RecipeList from '../Component/RecipeList';
+import RecipeDetails from '../Component/RecipeDetails';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       <Hero>
-        <Banner title="The Vegan Kitchen: Nourishing Your Body and Soul with Plant-Based Recipes" 
-        subtile="Discover the Joy of Cooking Compassionate, Wholesome Meals for a Healthier You and a Better Planet">
-        <Link to='/rooms' className="btn-primary">
-          Go to our recipes
-        </Link>          
+        <Banner
+          title="The Vegan Kitchen: Nourishing Your Body and Soul with Plant-Based Recipes"
+          subtile="Discover the Joy of Cooking Compassionate, Wholesome Meals for a Healthier You and a Better Planet"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Go to our recipes
+          </Link>
         </Banner>
       </Hero>
       <TopMeals />
       <MealList />
       <h1>Recipe List</h1>
       <Routes>
-        <Route exact path="/" component={RecipeList} />
-        <Route path="/recipe/:id" component={RecipeDetails} />
-      <Routes/>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
