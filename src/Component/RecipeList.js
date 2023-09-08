e58c31d781d627057e90aa7ce16bef6a14735943
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2023-09-08 17:02:13 +0200

    Update RecipeList.js

--- src/Component/RecipeList.js
+++ src/Component/RecipeList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'; // Import Link
-import { fetchRecipes } from './api';
+import { fetchRecipes } from '../api';
 import Loading from './Loading';
 
 function RecipeList() {
