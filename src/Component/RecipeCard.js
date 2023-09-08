8940c9ac043bab41c91081fdc07bb8b317f5723f
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2023-09-08 16:40:04 +0200

    Update RecipeCard.js

--- src/Component/RecipeCard.js
+++ src/Component/RecipeCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const RecipeCard = ({ title, image, instructions }) => {
+const RecipeCard = ({ title, image, id }) => {
   return (
-    <div className="recipe-card">
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{instructions}</p>
-    </div>
+    <Link to={`/recipe/${id}`} className="recipe-card-link">
+      <div className="recipe-card">
+        <img src={image} alt={title} />
+        <div className="recipe-card-body">
+          <h5 className="recipe-title">{title}</h5>
+        </div>
+      </div>
+    </Link>
   );
 };
 
