8ad3ab0527918fd30e6276d306ab186e2ed80787
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2023-09-14 19:58:29 +0200

    Update with error handling RecipeList.js

--- src/Component/RecipeList.js
+++ src/Component/RecipeList.js
@@ -5,11 +5,18 @@ import Loading from './Loading';
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getRecipes = async () => {
-      const data = await fetchRecipeById();
-      setRecipes(data);
+      try {
+        const data = await fetchRecipeById();
+        setRecipes(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+        setLoading(false);
+      }
     };
 
     getRecipes();
@@ -17,26 +24,24 @@ function RecipeList() {
 
   return (
     <div className="recipe-list">
-      {recipes ? (
-        recipes.length > 0 ? (
-          recipes.map((recipe, index) => (
-            <article className="meal" key={index}>
-              <div className="featured-meals-center">
-                <div className="img-container">
-                  {/* Use Link to navigate to the recipe details page */}
-                  <Link to={`/recipe/${recipe.id}`}>
-                    <img src={recipe.image} alt="Meal" />
-                    <p className="meal-info">{recipe.title}</p>
-                  </Link>
-                </div>
-              </div>
-            </article>
-          ))
-        ) : (
-          <p>No recipes available.</p> // Display a message when no recipes are available
-        )
-      ) : (
+      {loading ? (
         <Loading />
+      ) : recipes.length > 0 ? (
+        recipes.map((recipe, index) => (
+          <article className="meal" key={index}>
+            <div className="featured-meals-center">
+              <div className="img-container">
+                {/* Use Link to navigate to the recipe details page */}
+                <Link to={`/recipe/${recipe.id}`}>
+                  <img src={recipe.image} alt="Meal" />
+                  <p className="meal-info">{recipe.title}</p>
+                </Link>
+              </div>
+            </div>
+          </article>
+        ))
+      ) : (
+        <p>No recipes available.</p>
       )}
     </div>
   );
