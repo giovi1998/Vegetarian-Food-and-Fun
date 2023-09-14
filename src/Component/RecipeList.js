1486919957126d4399d3dd3fbcb1e07d78d0ab99
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2023-09-14 20:03:29 +0200

    Update RecipeList.js with different fetchapi

--- src/Component/RecipeList.js
+++ src/Component/RecipeList.js
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link
-import { fetchRecipeById } from '../api';
-import Loading from './Loading';
+import { fetchRecipeList } from '../api'; // Import the function to fetch the list of recipes
+import RecipeCard from './RecipeCard'; // Import the RecipeCard component
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getRecipes = async () => {
       try {
-        const data = await fetchRecipeById();
+        const data = await fetchRecipeList(); // Assuming you have a function to fetch the list of recipes
         setRecipes(data);
-        setLoading(false);
       } catch (error) {
         console.error('Error fetching recipes:', error);
-        setLoading(false);
       }
     };
 
@@ -24,25 +20,14 @@ function RecipeList() {
 
   return (
     <div className="recipe-list">
-      {loading ? (
-        <Loading />
-      ) : recipes.length > 0 ? (
-        recipes.map((recipe, index) => (
-          <article className="meal" key={index}>
-            <div className="featured-meals-center">
-              <div className="img-container">
-                {/* Use Link to navigate to the recipe details page */}
-                <Link to={`/recipe/${recipe.id}`}>
-                  <img src={recipe.image} alt="Meal" />
-                  <p className="meal-info">{recipe.title}</p>
-                </Link>
-              </div>
-            </div>
-          </article>
-        ))
-      ) : (
-        <p>No recipes available.</p>
-      )}
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          title={recipe.title}
+          image={recipe.image}
+          id={recipe.id}
+        />
+      ))}
     </div>
   );
 }
